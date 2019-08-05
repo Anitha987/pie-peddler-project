e4fe443dc2b53359d5a69fe1b7ee3fc90d6d611d
Anitha987 <anithaumuhire@gmail.com>
2019-08-05 22:00:09 +0200

    add paragraph

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function yourChoice(size,crust,toppings,quantity){
+this.pizzaSize=size;
+this.pizzaCrust=crust;
+this.pizzaToppings=toppings;
+this.pizzaQuantity=quantity;
+}
+yourChoice.prototype.fullpackage=function(){
+    return "Your order is"+this.pizzaSize+""+this.pizzaCrust+""+this.pizzaToppings +"" +this.Quantity+""
+}
+$(document).ready(function(){
+    $("#submit").click(function(event){
+    event.preventDefault();
+    var pizzaSize=$("select#wendy").val();
+    var pizzaCrust=$("select#anitha").val();
+    var pizzaToppings=$("select#rwaka").val();
+    var pizzaQuantity=$("select#esther").val();
+    if((pizzaSize=='small')&&(pizzaCrust=='Fast food pizza')&&(pizzaToppings=='mushrooms')){
+        newCost=500;
+        var newSum=newCost*Quantity;
+    }
+    });
+});
