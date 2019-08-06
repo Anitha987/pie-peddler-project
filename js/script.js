dc4aa99b8992c710c8194d80218e48ad24bd05bd
Anitha987 <anithaumuhire@gmail.com>
2019-08-06 10:14:52 +0200

    add head

--- js/script.js
+++ js/script.js
@@ -7,16 +7,61 @@ this.pizzaQuantity=quantity;
 yourChoice.prototype.fullpackage=function(){
     return "Your order is"+this.pizzaSize+""+this.pizzaCrust+""+this.pizzaToppings +"" +this.Quantity+""
 }
-$(document).ready(function(){
+
+
+$(document).ready(function() {
     $("#submit").click(function(event){
     event.preventDefault();
     var pizzaSize=$("select#wendy").val();
+    console.log(pizzaSize);
     var pizzaCrust=$("select#anitha").val();
     var pizzaToppings=$("select#rwaka").val();
     var pizzaQuantity=$("select#esther").val();
     if((pizzaSize=='small')&&(pizzaCrust=='Fast food pizza')&&(pizzaToppings=='mushrooms')){
         newCost=500;
-        var newSum=newCost*Quantity;
+        var newSum=newCost*pizzaQuantity;
+        console.log(newSum);    
     }
+    if((pizzaSize=='small')&&(pizzaCrust=='ny style')&&(pizzaToppings=='onions')){
+        newCost=800;
+        var newSum=newCost*pizzaQuantity;
+        // console.log(newSum);
+    }
+    if((pizzaSize=='small')&&(pizzaCrust=='pizza bigels')&&(pizzaToppings=='sausage')){
+        newCost=1000;
+        var newSum=newCost*pizzaQuantity;
+        console.log(newSum);
+    }
+    if((pizzaSize=='medium')&&(pizzaCrust=='Fast food pizza')&&(pizzaToppings=='bacon')){
+        newCost=700;
+        var newSum=newCost*pizzaQuantity;
+        console.log(newSum);
+    }
+    if((pizzaSize=='large')&&(pizzaCrust=='Fthin crust')&&(pizzaToppings=='extra cheese')){
+        newCost=600;
+        var newSum=newCost*pizzaQuantity;
+        console.log(newSum);
+    }
+    if((pizzaSize=='x-large')&&(pizzaCrust=='chicago')&&(pizzaToppings=='black olives')){
+        newCost=900;
+        var newSum=newCost*pizzaQuantity;
+        console.log(newSum);
+    }
+    if((pizzaSize=='medium')&&(pizzaCrust=='Fflat bread')&&(pizzaToppings=='green peppers')){
+        newCost=800;
+        var newSum=newCost*pizzaQuantity;
+        console.log(newSum);
+    }
+    if((pizzaSize=='large')&&(pizzaCrust=='napolian')&&(pizzaToppings=='spinach')){
+        newCost=700;
+        var newSum=newCost*pizzaQuantity;
+        console.log(newSum);
+    }
+    if((pizzaSize=='x-large')&&(pizzaCrust=='silician')&&(pizzaToppings=='pineapple')){
+        newCost=1000;
+        var newSum=newCost*pizzaQuantity;
+        console.log(newSum);
+    }
+    // $("h3#house").append("");
     });
 });
